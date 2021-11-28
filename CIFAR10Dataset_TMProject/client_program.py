23fa50f3614967821d9ecf7089f9b53dc3302b7d
Rahul Bhati <rahulbhati0042@gmail.com>
2021-11-28 22:22:08 +0530

    Assignment_Tensorflow_CIFAR10_Image Pradiction

--- CIFAR10Dataset_TMProject/client_program.py
+++ CIFAR10Dataset_TMProject/client_program.py
@@ -1,5 +1,12 @@
 import sys
 import requests as req
+from PIL import Image
+from PIL import ImageDraw
+import json
+import re
+import os
+import PIL.Image
+import matplotlib.font_manager
 
 
 # total arguments
@@ -7,7 +14,23 @@ n = len(sys.argv)
 print("Total arguments passed:", n)
 # Arguments passed
 print("\nName of Python script:", sys.argv[0])
-
+listOfFonts = matplotlib.font_manager.win32InstalledFonts()[:1]
 print("\nFolder path:", sys.argv[1])
+savedPath="G:\ThinkingMachine\WorkspaceTMMachinLearning\CIFAR10Dataset_TMProject\predictedImg"
+print("\nPredicted Image Folder Path :",savedPath)
 resp = req.request(method='POST', url="http://localhost:1001/uploadImages", data = sys.argv[1])
-print(resp.text)
+test=re.sub('\'','\"',resp.text)
+dir = json.loads(test)
+
+for key, value in dir.items():
+    imgPath = os.path.join(sys.argv[1],key)
+    img = Image.open(imgPath)
+    toolBox = PIL.ImageDraw.Draw(img)
+    x, y = 0,25
+    font = PIL.ImageFont.truetype("G:\ThinkingMachine\WorkspaceTMMachinLearning\CIFAR10Dataset_TMProject\OpenSans-Bold.ttf", size=7)
+    w, h = font.getsize(value)
+    xx = 32-(x + w)
+    toolBox.rectangle((xx, y, xx + w, y + h), fill='white')
+
+    toolBox.text((xx, 22), value, fill='black',font=font)
+    img.save(os.path.join(savedPath,value+"_"+key))
--- CIFAR10Dataset_TMProject/upload_images.py
+++ CIFAR10Dataset_TMProject/upload_images.py
@@ -1,47 +1,47 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
 import os
-from os import listdir
-from os.path import isfile, join
-import io
 import cv2
-from keras.preprocessing.image import load_img
+import tensorflow as tf
+
+from keras.models import load_model
+import numpy as np
 
 class helloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path.endswith('/uploadImages'):
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write('upload Images'.encode())
-
-        else:
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write('Hello Control'.encode())
 
     def do_POST(self):
+         cifar_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
          if self.path.endswith('/uploadImages'):
             content_length = int(self.headers['Content-Length'])
             body = (self.rfile.read(content_length)).decode('utf-8')
             
             files = os.listdir(body)
-            
+            model =load_model('G:\ThinkingMachine\WorkspaceTMMachinLearning\CIFAR10Dataset_TMProject\model\my_first_model.h5')
+ 
+            print('@@ Model loaded')
+            dir_Response = {}
             for f in files:
-                
                 imagefile=os.path.join(body, f)
                 print("filane name :",imagefile)
                 imageData= cv2.imread(imagefile)
-                print(imageData)
-                #imagefile1=os.path.join("CIFAR10Dataset_TMProject/imgs/", f)
-                #print("file name new :",imagefile1)
-                cv2.imwrite("D:/RahulBhati/Machine_Learning_Data/ThinkingMachine/WorkspaceTMMachinLearning/CIFAR10Dataset_TMProject/imgs/"+f,imageData)
-                            
+                cv2.imwrite("G:/ThinkingMachine/WorkspaceTMMachinLearning/CIFAR10Dataset_TMProject/imgs/"+f,imageData)
+                #test_image = load_img("G:/ThinkingMachine/WorkspaceTMMachinLearning/CIFAR10Dataset_TMProject/imgs/"+f) # load image 
+                print("@@ Got Image for prediction")
+                predImageData=imageData.reshape(1,32*32*3)
+                predImageData = (predImageData-np.mean(predImageData))/np.std(predImageData)
+                pred = model.predict(predImageData) 
+                print("Predicted Result :",pred)
+                pred = np.argmax(pred)
+                print("Predicted Data :",pred)
+                print("Predicted Label:",cifar_classes[pred])
+                dir_Response[f]=cifar_classes[pred]
+
             
+                
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
-            self.wfile.write('Post upload Images'.encode())
+            self.wfile.write(str(dir_Response).encode())
         
          else:
             self.send_response(200)
@@ -49,8 +49,6 @@ class helloHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write('Post Hello Control'.encode())
 
-
-           
 
 def main():
     PORT = 1001
